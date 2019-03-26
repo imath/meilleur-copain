f04cde62d2a2b86c542ff33066efee0af150dfac
imath <math.viet@gmail.com>
2019-03-26 17:46:13 +0100

    Allow thumbnail for directories and alignment for all components.

--- inc/functions.php
+++ inc/functions.php
@@ -10,46 +10,6 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-
-function meilleur_copain_set_thumbnail( $html = '', $post_ID = 0, $post_thumbnail_id = 0, $size = '', $attr = '' ) {
-    // Prevent infinite loops.
-    remove_filter( 'post_thumbnail_html', 'meilleur_copain_set_thumbnail' );
-
-    $bp = buddypress();
-    $mc = meilleur_copain();
-
-    if ( isset( $mc->post->object ) && $mc->post->object ) {
-        $mc->post->ID = (int) $bp->pages->{$mc->post->object}->id;
-    }
-
-    // Restore the page thumbnail.
-    return get_the_post_thumbnail( $mc->post, $size, $attr );
-}
-
-function meilleur_copain_activity_index() {
-    $mc               = meilleur_copain();
-    $mc->post         = get_post();
-    $mc->post->object = 'activity';
-
-    if ( has_post_thumbnail( $mc->post ) ) {
-        add_filter( 'has_post_thumbnail', '__return_true' );
-        add_filter( 'post_thumbnail_html', 'meilleur_copain_set_thumbnail', 10, 5 );
-    }
-
-    if ( function_exists( 'has_block' ) && has_block( 'meilleur-copain/placeholder', $mc->post ) ) {
-        $blocks = parse_blocks( $mc->post->post_content );
-        $block  = reset( $blocks );
-
-        // Set Activity's page attributes.
-        buddypress()->activity->page_attributes = wp_parse_args( $block['attrs'],
-            array(
-                'align' => '',
-            )
-        );
-    }
-}
-add_action( 'bp_activity_screen_index', 'meilleur_copain_activity_index' );
-
 function meilleur_copain_register_placeholder_block() {
     wp_register_script(
         'meilleur-copain-placeholder',
@@ -62,20 +22,6 @@ function meilleur_copain_register_placeholder_block() {
 	) );
 }
 add_action( 'init', 'meilleur_copain_register_placeholder_block' );
-
-function meilleur_copain_template_pack_container_classes( $class = '' ) {
-    if ( bp_is_directory() ) {
-        $bp                = buddypress();
-        $current_component = bp_current_component();
-
-        if ( isset( $bp->{$current_component}->page_attributes['align'] ) ) {
-            $class .= ' align' . $bp->{$current_component}->page_attributes['align'];
-        }
-    }
-
-    return $class;
-}
-add_filter( 'bp_nouveau_get_container_classes', 'meilleur_copain_template_pack_container_classes' );
 
 function meilleur_copain_set_blocks_template() {
     if ( ! isset( $_GET['post'] ) ) {
@@ -98,6 +44,85 @@ function meilleur_copain_set_blocks_template() {
     }
 }
 add_action( 'load-page.php', 'meilleur_copain_set_blocks_template' );
+
+function meilleur_copain_set_thumbnail( $html = '', $post_ID = 0, $post_thumbnail_id = 0, $size = '', $attr = '' ) {
+    // Prevent infinite loops.
+    remove_filter( 'post_thumbnail_html', 'meilleur_copain_set_thumbnail' );
+
+    $mc = meilleur_copain();
+
+    if ( ! isset( $mc->post ) || ! $mc->post->ID ) {
+        return $html;
+    }
+
+    // Restore the page thumbnail.
+    return get_the_post_thumbnail( $mc->post, $size, $attr );
+}
+
+function meilleur_copain_theme_compat() {
+    $bp               = buddypress();
+    $mc               = meilleur_copain();
+
+    if ( bp_is_group() ) {
+        $bp_object = 'groups';
+    } elseif ( bp_is_user() ) {
+        $bp_object = 'members';
+    } else {
+        $bp_object = bp_current_component();
+    }
+
+    if ( ! $bp_object || ! isset( $bp->pages->{$bp_object}->id ) ) {
+        return;
+    }
+
+    // Get the cached post.
+    $mc->post = WP_POST::get_instance( $bp->pages->{$bp_object}->id );
+
+    // Use the page thumbnail only for BuddyPress directories.
+    if ( bp_is_directory() && has_post_thumbnail( $mc->post ) ) {
+        add_filter( 'has_post_thumbnail', '__return_true' );
+        add_filter( 'post_thumbnail_html', 'meilleur_copain_set_thumbnail', 10, 5 );
+    }
+}
+add_action( 'bp_setup_theme_compat', 'meilleur_copain_theme_compat', 100 );
+
+function meilleur_copain_get_container_class() {
+    $mc = meilleur_copain();
+    $class = '';
+
+    if ( ! isset( $mc->post ) ) {
+        return $class;
+    }
+
+    if ( function_exists( 'has_block' ) && has_block( 'meilleur-copain/placeholder', $mc->post ) ) {
+        $blocks = parse_blocks( $mc->post->post_content );
+        $block  = reset( $blocks );
+
+        // Get page attributes.
+        $attributes = wp_parse_args( $block['attrs'],
+            array(
+                'align' => '',
+            )
+        );
+
+        if ( $attributes['align'] ) {
+            $class = 'align' . $attributes['align'];
+        }
+    }
+
+    return apply_filters( 'meilleur_copain_get_container_class', $class );
+}
+
+function meilleur_copain_template_pack_container_classes( $class = '' ) {
+    $mc_class = meilleur_copain_get_container_class();
+
+    if ( ! $mc_class ) {
+        return $class;
+    }
+
+    return $class . ' ' . sanitize_html_class( $mc_class );
+}
+add_filter( 'bp_nouveau_get_container_classes', 'meilleur_copain_template_pack_container_classes' );
 
 /**
  *
