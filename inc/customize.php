75223db35819a4af778db94bcd82672205d38504
imath <math.viet@gmail.com>
2019-04-07 08:55:32 +0200

    Improve inline docs for inc/customize.php

--- inc/customize.php
+++ inc/customize.php
@@ -10,6 +10,14 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Adds the Custom CSS option to Nouveau customizer options.
+ *
+ * @since 1.0.0
+ *
+ * @param  array $args The BP Nouveau customizer options.
+ * @return array       The BP Nouveau customizer options.
+ */
 function meilleur_copain_custom_css_option( $args = array() ) {
 	$custom_css = '';
 
@@ -23,6 +31,14 @@ function meilleur_copain_custom_css_option( $args = array() ) {
 }
 add_filter( 'bp_after_nouveau_appearance_settings_parse_args', 'meilleur_copain_custom_css_option' );
 
+/**
+ * Adds the Custom CSS section to Nouveau customizer sections.
+ *
+ * @since 1.0.0
+ *
+ * @param  array $sections The BP Nouveau customizer sections.
+ * @return array           The BP Nouveau customizer sections.
+ */
 function meilleur_copain_customizer_sections( $sections = array() ) {
     return array_merge( $sections, array(
         'meilleur_copain_custom_css_section' => array(
@@ -35,6 +51,14 @@ function meilleur_copain_customizer_sections( $sections = array() ) {
 }
 add_filter( 'bp_nouveau_customizer_sections', 'meilleur_copain_customizer_sections' );
 
+/**
+ * Adds the Custom CSS setting to Nouveau customizer settings.
+ *
+ * @since 1.0.0
+ *
+ * @param  array $settings The BP Nouveau customizer settings.
+ * @return array           The BP Nouveau customizer settings.
+ */
 function meilleur_copain_customizer_settings( $settings = array() ) {
     return array_merge( $settings, array(
         'bp_nouveau_appearance[meilleur_copain_custom_css]' => array(
@@ -48,6 +72,14 @@ function meilleur_copain_customizer_settings( $settings = array() ) {
 }
 add_filter( 'bp_nouveau_customizer_settings', 'meilleur_copain_customizer_settings' );
 
+/**
+ * Adds the Custom CSS control to Nouveau customizer controls.
+ *
+ * @since 1.0.0
+ *
+ * @param  array $controls The BP Nouveau customizer controls.
+ * @return array           The BP Nouveau customizer controls.
+ */
 function meilleur_copain_customizer_controls( $controls = array() ) {
 	return array_merge( $controls, array(
 		'meilleur_copain_custom_css' => array(
@@ -62,6 +94,11 @@ function meilleur_copain_customizer_controls( $controls = array() ) {
 }
 add_filter( 'bp_nouveau_customizer_controls', 'meilleur_copain_customizer_controls' );
 
+/**
+ * Adds a script to preview CSS code changes.
+ *
+ * @since 1.0.0
+ */
 function meilleur_copain_customize_preview_js() {
 	$mc = meilleur_copain();
 
@@ -75,6 +112,11 @@ function meilleur_copain_customize_preview_js() {
 }
 add_action( 'customize_preview_init', 'meilleur_copain_customize_preview_js' );
 
+/**
+ * Adds an inline style to improve CodeMirror display.
+ *
+ * @since 1.0.0
+ */
 function meilleur_copain_controls_enqueue_scripts() {
 	wp_add_inline_style( 'customize-controls', '
 		.customize-section-description-container + #customize-control-meilleur_copain_custom_css:last-child .CodeMirror {
